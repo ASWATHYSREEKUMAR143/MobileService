bdeabfce4c73a583aa2ce027adcf9c245fc9831b
ammu0 <ammu0@LAPTOP-O0AAK5TJ.bbrouter>
2023-05-31 02:45:53 +0530

    login commit

--- ObsFrramework/src/test/java/com/test/LoginTest.java
+++ ObsFrramework/src/test/java/com/test/LoginTest.java
@@ -1,46 +1,34 @@
 package com.test;
 
 import java.util.Properties;
-
-import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-
 import com.base.AutomationBase;
 import com.pages.HomePage;
 import com.pages.LoginPage;
 import com.utilities.PropertyUtils;
-import com.utilities.WebBrowserUtils;
 
-public class LoginTest extends AutomationBase{
-	//WebDriver driver;	
-	WebBrowserUtils webbrowser;	
+public class LoginTest extends AutomationBase {
 	LoginPage login;
 	HomePage home;
 	PropertyUtils propertyutil;
 	Properties prop;
-	
-	
 
-	
 	@Test(retryAnalyzer = com.analyzer.RetryAnalyzer.class)
-	public void validateLogin() throws Exception
-	{
-		//driver=getDriver();
-		login=new LoginPage(driver);
-		home=new HomePage(driver);
-		propertyutil=new PropertyUtils();
-		prop = PropertyUtils.getProperty("config.properties");		
+	public void validateLogin() throws Exception {
+		login = new LoginPage(driver);
+		home = new HomePage(driver);
+		propertyutil = new PropertyUtils();
+		prop = PropertyUtils.getProperty("config.properties");
 		login.performLogin(prop.getProperty("username"), prop.getProperty("password"));
-	Assert.assertTrue(home.isSuccessPopupDisplayed(),"Failure message: Unable to Login");
+		Assert.assertTrue(home.isSuccessPopupDisplayed(), "Failure message: Unable to Login");
 	}
-	
+
 	@AfterMethod
-	
-	public void postRun()
-	{
-		
+
+	public void postRun() {
+
 		home.toLogOut();
 	}
 
