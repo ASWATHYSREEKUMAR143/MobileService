f2766ca5c4167760b83e90b474d4d9a987bec09d
ammu0 <ammu0@LAPTOP-O0AAK5TJ.bbrouter>
2023-05-31 02:46:31 +0530

    quantity commit

--- ObsFrramework/src/test/java/com/test/QuantityAlertPageTest.java
+++ ObsFrramework/src/test/java/com/test/QuantityAlertPageTest.java
@@ -2,12 +2,9 @@ package com.test;
 
 import java.util.Properties;
 
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-
 import com.base.AutomationBase;
 import com.constants.AutomationConstants;
 import com.pages.HomePage;
@@ -16,14 +13,8 @@ import com.pages.QuantityAlertPage;
 import com.utilities.ExcelUtils;
 import com.utilities.PropertyUtils;
 import com.utilities.WaitUtils;
-import com.utilities.WebBrowserUtils;
 
 public class QuantityAlertPageTest extends AutomationBase {
-
-	// WebDriver driver;
-
-	WebBrowserUtils webbrowser;
-
 	LoginPage login;
 	HomePage home;
 	QuantityAlertPage qtyalrt;
@@ -31,40 +22,16 @@ public class QuantityAlertPageTest extends AutomationBase {
 	Properties prop;
 	ExcelUtils excelutils;
 	WaitUtils wait;
-	AutomationConstants constants;
-
-	/*@BeforeMethod
-	public void preRun() throws Exception {
-		// driver = getDriver();
-		login = new LoginPage(driver);
-		// webbrowser = new WebBrowserUtils();
-		home = new HomePage(driver);
-		qtyalrt = new QuantityAlertPage(driver);
-		wait = new WaitUtils();
-		// constants = new AutomationConstants();
-		propertyutil = new PropertyUtils();
-		prop = PropertyUtils.getProperty("config.properties");
-		excelutils = new ExcelUtils();
-
-		home = login.login(prop.getProperty("username"), prop.getProperty("password"));
-		qtyalrt = home.quantityalert();
-		wait.toApplyImplicitWait(driver, 10);
-
-	}*/
 
 	@Test(enabled = true, priority = 1)
 	public void validateQuantityAlertPageFields() {
-		// driver = getDriver();
 		login = new LoginPage(driver);
-		// webbrowser = new WebBrowserUtils();
 		home = new HomePage(driver);
 		qtyalrt = new QuantityAlertPage(driver);
 		wait = new WaitUtils();
-		// constants = new AutomationConstants();
 		propertyutil = new PropertyUtils();
 		prop = PropertyUtils.getProperty("config.properties");
 		excelutils = new ExcelUtils();
-
 		home = login.login(prop.getProperty("username"), prop.getProperty("password"));
 		qtyalrt = home.quantityalert();
 		wait.toApplyImplicitWait(driver, 10);
